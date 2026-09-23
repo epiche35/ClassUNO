97e1173d591449ff25f6a482ff6ddd69c5df3257
Arun Yadav <epiche52@gmail.com>
2026-09-24 01:37:22 +0545

    Save and deploy final logo update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// Import logo and set it as the browser favicon dynamically
+import logoUrl from './assets/images/logo.png';
+
+const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+link.type = 'image/png';
+link.rel = 'icon';
+link.href = logoUrl;
+document.head.appendChild(link);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 );
