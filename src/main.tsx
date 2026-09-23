0807a6d3f0f9d78ee52dc00fb80bb7693c99f2d7
Arun Yadav <epiche52@gmail.com>
2026-09-24 01:59:35 +0545

    Remove conflicting dynamic favicon code

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// Dynamically import logo and set it as the browser tab favicon
-import logoUrl from './assets/images/logo.png';
-
-const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-link.type = 'image/png';
-link.rel = 'icon';
-link.href = logoUrl;
-document.head.appendChild(link);
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(getElementByIdOrThrow()).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+function getElementByIdOrThrow() {
+  const el = document.getElementById('root');
+  if (!el) throw new Error('Root element not found');
+  return el;
+}
